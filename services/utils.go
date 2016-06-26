e9725805a5ba8de243cd4155f8974c7366c52fe4
YanXin-Lotus <rin_lotus@live.com>
2016-06-26 18:40:17 +0800

    views edit

--- services/utils.go
+++ services/utils.go
@@ -15,6 +15,6 @@ func Md5(origin string) string {
 func SendMail(to string, sub string, body string) (err error) {
 	auth := smtp.PlainAuth("", "user@example.com", "password", "mail.example.com")
 	msg := []byte("To: " + to + "\n" + "Subject: " + sub + "\r\n" + "\r\n" + body + "\r\n")
-	err = smtp.SendMail("mail.example.com:25", auth, "sender@example.com", to, msg)
+	err = smtp.SendMail("mail.example.com:25", auth, "sender@example.com", []string{to}, msg)
 	return err
 }
